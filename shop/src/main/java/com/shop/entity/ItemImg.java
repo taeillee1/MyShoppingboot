d9417ab45fe158112433eb20a6cb61af6dac734d
taeillee <leemsdong2@naver.com>
2022-02-05 17:27:31 +0900

    1

--- shop/src/main/java/com/shop/entity/ItemImg.java
+++ shop/src/main/java/com/shop/entity/ItemImg.java
@@ -1,4 +1,37 @@
 package com.shop.entity;
 
-public class ItemImg {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity{
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;//원본이미지 파일명
+
+    private String imgUrl;// 이미지조회경로
+
+    private String repImgYn;// 대표이미지 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+        this.oriImgName=oriImgName;
+        this.imgName=imgName;
+        this.imgUrl=imgUrl;
+    }
+
 }
